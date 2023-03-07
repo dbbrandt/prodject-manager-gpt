986149e0f3439cd24cee7f61f6f1fada995098c0
Daniel Brandt <dbbrandt@gmail.com>
2023-03-07 12:48:06 -0500

    Inproved history output and responsiveness.

--- pages/components/ui/HistoryList.js
+++ pages/components/ui/HistoryList.js
@@ -1,12 +1,15 @@
 import React, {useContext} from 'react';
 import AppContext from "../utils/appContext";
+import styles from "../../index.module.css";
 
 function HistoryList() {
     const {history} = useContext(AppContext);
     if (history.length === 0) return;
     return (
         <ul>
-            {history.map((e) => <li>{e.question} Answer: {e.answer}</li>)}
+            {history.map((e) => <li>
+                <div className={styles.historyQuestion}>{e.question}</div>
+                <div className={styles.historyAnswer}>{e.answer}</div></li>)}
         </ul>
     )
 }
