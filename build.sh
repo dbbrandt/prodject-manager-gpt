0d152dc2f42c6a86f94db89bd8d7e8176279609b
Daniel Brandt <dbbrandt@gmail.com>
2023-03-12 12:18:25 -0400

    Working on EB deploy

---
+++ build.sh
@@ -0,0 +1,2 @@
+export NODE_ENV=production
+zip -r app.zip .next pages utils public styles *.json *.sh .npmrc
--- ops.sh
+++ ops.sh
@@ -1,6 +1,3 @@
-ps -ef | grep 8080
-export OPENAI_API_KEY=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
 npm run build
-nohup npm run start -- --port 8080 &
-tail -f nohup.out
-
+npm run start -- --port  8080
+lsof -iTCP -sTCP:LISTEN -P
